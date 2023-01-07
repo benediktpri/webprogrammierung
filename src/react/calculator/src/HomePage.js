3b261a5b8a9c5520864ae43660b8d612b0cf3472
benediktpri <felix.2001@web.de>
2023-01-07 22:15:24 +0000

    Hompage geupdated

--- src/react/calculator/src/HomePage.js
+++ src/react/calculator/src/HomePage.js
@@ -2,6 +2,8 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './home.css';
 import React from 'react';
 
+import { Nav, Navbar } from 'react-bootstrap';
+
 import { Link } from 'react-router-dom';
 
 function HomePage() {
@@ -10,9 +12,12 @@ function HomePage() {
   var Map = require('./img/map.jpg');
   var Willy = require('./img/willy-abi.jpg');
 
+
   return (
+
+
     <div>
-      <nav className="navbar navbar-expand-lg bg-light">
+      <nav collapseOnSelect expand="lg" class="navbar navbar-expand-lg bg-light">
         <div className="container-fluid">
           <a className="navbar-brand" href="#">WankyWombat</a>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
@@ -51,53 +56,64 @@ function HomePage() {
           </div>
         </div>
       </nav>
+
       <div className="hero">
-        <div id="main" className="container">
+        <div id="main" className="container-fluid">
           <div className="row">
             <div id="titel" className="col-12">WankyWombat</div>
+          </div>
+          <div className="row">
             <div id="claim" className="col-12">Report Dangerous Animals.<br /> Instantly.</div>
           </div>
-
         </div>
-      </div><div className="cards">
+      </div>
+
+      <div className="cards container-fluid">
         <div className="row">
-          <div className="col-12 col-sm-12 col-lg-4 mt-sm-2 mt-0 mb-5 d-flex justify-content-center">
-            <div className="card" style={{ width: '18rem' }}>
+          <div className="col-12 col-sm-4 mt-sm-2 mt-0 mb-5 ">
+            <div className="card" >
               <img src={Wombat} className="card-img-top" alt="..." />
               <div className="card-body">
-                <h5 className="card-title">Report</h5>
+                <h3 className="card-title">Report</h3>
                 <p className="card-text">With the click of a button you can report any dangerous animals.</p>
                 <Link to="/report">
-                  <button className="btn btn-primary d-flex justify-content-center">Report a dangerous animal.</button>
+                  <a className="btn btn-primary d-flex justify-content-center">Report a dangerous animal.</a>
+                </Link>
+              </div>
+
+            </div>
+          </div>
+          <div className="col-12 col-sm-4 mt-sm-2 mt-0 mb-5  ">
+            <div className="card" >
+              <img src={Map} className="card-img-top" alt="..." />
+              <div className="card-body">
+                <h3 className="card-title">Realtime</h3>
+                <p className="card-text">See a realtime overview of all the reports in your area.</p>
+                <Link to="/map">
+                  <a className="btn btn-primary d-flex justify-content-center">Go to map</a>
                 </Link>
               </div>
             </div>
           </div>
-          <div className="col-12 col-sm-12 col-lg-4 mt-sm-2 mt-0 mb-5 d-flex justify-content-center">
-            <div className="card" style={{ width: '18rem' }}>
-              <img src={Map} className="card-img-top" alt="..." />
-              <div className="card-body">
-                <h5 className="card-title">Realtime</h5>
-                <p className="card-text">See a realtime overview of all the reports in your area.</p>
-                <a href="maps.html" className="btn btn-primary d-flex justify-content-center">Go to map</a>
-              </div>
-            </div>
-          </div>
-          <div className="col-12 col-sm-12 col-lg-4 mt-sm-2 mt-0 mb-5 d-flex justify-content-center">
-            <div className="card" style={{ width: '18rem' }}>
+          <div className="col-12 col-sm-4 mt-sm-2 mt-0 mb-5  ">
+            <div className="card" >
               <img src={Willy} className="card-img-top" alt="..." />
               <div className="card-body">
-                <h5 className="card-title">Rangers</h5>
+                <h3 className="card-title">Rangers</h3>
                 <p className="card-text">A ranger will be notified and remove the danger.</p>
-                <a href="list.html" className="btn btn-primary d-flex justify-content-center">Go to the list of reports.</a>
+                <Link to="/list">
+                  <a className="btn btn-primary d-flex justify-content-center">Go to the list of reports.</a>
+                </Link>
               </div>
             </div>
           </div>
         </div>
-      </div><script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
+      </div>
+
+
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4"
         crossOrigin="anonymous"></script>
-
     </div>
   );
 }
