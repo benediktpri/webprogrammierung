2019d5fb92bac2f5a45e7ab294d883e1f096931b
benediktpri <felix.2001@web.de>
2023-01-11 12:01:12 +0000

    Email of reports

--- src/react/calculator/src/ReportPage.js
+++ src/react/calculator/src/ReportPage.js
@@ -6,6 +6,7 @@ import { useForm } from "react-hook-form";
 import { pushReport } from './DBConnector';
 import { Link } from 'react-router-dom';
 import { useState } from 'react';
+import emailjs from '@emailjs/browser';
 
 import { now, moment } from "moment";
 
@@ -13,9 +14,19 @@ function ReportPage() {
 
   const { register, handleSubmit, formState } = useForm();
 
+
   const onSubmit = (data) => {
     console.log(data);
     pushReport(data.tiername, ort_str, data.hinweis, now());
+    var mail_values = {
+      animal: data.tiername,
+      location: ort_str,
+      info: data.hinweis,
+      email: 'felix.2001@web.de' // Emailadress of ranger
+    };
+    /*e.preventDefau
+    /*emailjs.send('service_sxdzo4f', 'template_0o7vawb', mail_values, 'eY3vlgIfp7iXz3CD0');*/
+    console.log("Emails send")
   }
 
   //const [latitude, setLat] = useState('LongitudeDefault');
